fb621cf8f159974462956ea126712832120a09cb
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2026-01-29 22:05:11 +0400

    feat(act): add interface Act

--- src/app/act-form/act-form.ts
+++ src/app/act-form/act-form.ts
@@ -1,20 +1,44 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { JsonPipe } from "@angular/common";
+
+import { ActPreview } from '../act-preview/act-preview';
+import { Act } from "../models/act.model"
 
 @Component({
   standalone: true,
   selector: 'app-act-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ActPreview, JsonPipe],
   templateUrl: './act-form.html',
   styleUrls: ['./act-form.css'],
 })
+
 export class ActForm {
   fio = '';
   address = '';
-
+  date = '';
+  executor = ''
+  acts: Act[] = [];
 
   submit() {
-    console.log ("ФИО: ", this.fio);
-    console.log ("Адрес: ", this.address);
+    // console.log ("ФИО: ", this.fio);
+    // console.log ("Адрес: ", this.address);
+
+    const newAct: Act = {
+      fio: this.fio,
+      address: this.address,
+      date: this.date,
+      executor: this.executor,
+      createdAt: new Date()
+    }
+
+    this.acts.push(newAct)
+    console.log('Новый акт добавлен: ', newAct)
+
+    this.fio = '';
+    this.address = '';
+    this.date = '';
+    this.executor = '';
+
   }
 }
